71153228834f8058b9b2f639294eadce8efaf45e
felipe-andrade <felipe.astratta@gmail.com>
2024-06-07 19:46:52 -0300

    v1.1

--- cuzilloader.py
+++ cuzilloader.py
@@ -1,13 +1,32 @@
 import argparse
 from yt_dlp import YoutubeDL
 from platform import system
+from urllib.parse import urlparse
 
-def download_file(url: str, ext: str, home: str) -> None:
+def download_file(url: str, ext: str, home: str, quality: str) -> None:
     if ext == "mp4":
-        ydl_opts= {
-            "format": f"{ext}",
-            "outtmpl": f"{home}%(id)s.%(ext)s",
-        }
+        def extract_domain(url: str) -> str:
+            parsed_url = urlparse(url)
+            domain = parsed_url.netloc
+            if domain.startswith("www."):
+                domain = domain[4:]
+            if domain.endswith(".com"):
+                domain = domain[:-4]
+            return domain
+        
+        if quality == "worst":
+            ydl_opts= {
+                "format": "bv*[height<=480][ext=mp4]+ba[ext=m4a]/b[height<=480][ext=mp4] / wv*+ba/w",
+                "outtmpl": f"{home}%(id)s.%(ext)s",
+                "extractor": "twitter" if extract_domain(url) == "x" else extract_domain(url),
+            }
+        else:
+            ydl_opts= {
+                "format": "bv*[ext=mp4]+ba[ext=m4a]/b[ext=mp4] / bv*+ba/b",
+                "outtmpl": f"{home}%(id)s.%(ext)s",
+                "extractor": "twitter" if extract_domain(url) == "x" else extract_domain(url),
+            }
+
     else:
         ydl_opts = {
             "format": "bestaudio/best",
@@ -28,6 +47,7 @@ def argparser() -> argparse.Namespace:
     try:
         parser.add_argument("url", type=str)
         parser.add_argument("-f", "--format", type=str, nargs="?", default="mp4")
+        parser.add_argument("-q", "--quality", type=str, nargs="?", default="worst")
     except argparse.ArgumentError as e:
         print(e)
 
@@ -55,7 +75,7 @@ def set_home(system: str) -> str:
 def main():
     home = set_home(system())
     args = argparser()
-    download_file(args.url, args.format, home)
+    download_file(args.url, args.format, home, args.quality)
 
 if __name__ == "__main__":
     main()
